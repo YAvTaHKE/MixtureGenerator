008a4a8c55f45c73fc48f3aa0b5466cd23a15ba3
YAvTaHKE <moertiac@gmail.com>
2020-04-27 09:49:11 +0300

    Разработака интерфейса
    (cherry picked from commit bfecbc8055e96d45e04446af077b75d4fb9c367c)
    
    # Conflicts:
    #	src/main/java/GUI/MainFrame.java

--- src/main/java/ru/GUI/Utilities/HtmlStyler.java
+++ src/main/java/ru/GUI/Utilities/HtmlStyler.java
@@ -1,5 +1,7 @@
 package ru.GUI.Utilities;
 
+import java.awt.*;
+
 public class HtmlStyler {
     private StringBuffer sb;
 
@@ -9,7 +11,7 @@ public class HtmlStyler {
 
     public String getHtmlString() {
         sb.insert(0, "<html>");
-        sb.append("</html >");
+        sb.append("</html>");
 
         return sb.toString();
     }
@@ -17,13 +19,35 @@ public class HtmlStyler {
     //Форматирование по центру
     public void center(){
         sb.insert(0, "<center>");
-        sb.append("</center >");
+        sb.append("</center>");
     }
 
     //Форматирование жирным шрифтом
     public void bold(){
         sb.insert(0, "<b>");
-        sb.append("</b >");
+        sb.append("</b>");
     }
 
+    //Форматирование подчеркивание
+    public void underlined(){
+        sb.insert(0, "<u>");
+        sb.append("</u>");
+    }
+
+    //Вставка переноса на другую строку
+    public void breakLine(int offset){
+        sb.insert(offset, "<br>");
+    }
+
+    public void fontColor(Color color){
+        String hex = "#"+Integer.toHexString(color.getRGB()).substring(2);
+        sb.insert(0, "<font color=" + hex + ">");
+        sb.append("</font>");
+    }
+
+    //Форматирование курсивом
+    public void italic(){
+        sb.insert(0, "<em>");
+        sb.append("</em>");
+    }
 }
